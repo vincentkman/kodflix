f9d70fc112f65da270fd0e18f225c6cae70fc738
Vincent Man <vincentman2702@hotmail.com>
2019-06-22 15:41:20 +0100

    Enable the backend server in prod #23

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,9 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3001;
 const getShows = require('./shows');
 
 // Add route -> request and response
-app.get('/rest/shows', (req, res) => res.send(getShows()))
+app.get('/rest/shows', (req, res) => res.send(getShows()));
 
-app.listen(port, () => console.log(`Server started on port ${port}!`))
+app.listen(port, () => console.log(`Server started on port ${port}!`));
+
+// Serve any static files
+app.use(express.static(path.join(__dirname, '../../build')));
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
